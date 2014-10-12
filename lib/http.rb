6644e8376a5d595f98a727a5e6d9b8d84dcc6d24
Jordan A. Skarbek <jaskarbek@gmail.com>
2014-10-11 23:45:26 -0700

    added error handling for a file not being found

--- lib/http.rb
+++ lib/http.rb
@@ -31,15 +31,33 @@ loop do
 
   path = requested_file(request_line)
 
-  response = "Hello World!\n"
+  if File.exist?(path) && !File.directory?(path)
+    File.open(path, 'rb') do |file|
+      client.print('HTTP/1.1 200 OK\r\n' +
+                   'Content-Type: #{content_type(file)}\r\n' +
+                   'Content-Length: #{file.size}\r\n' +
+                   'Connection: close\r\n')
 
-  client.print("HTTP/1.1 200 OK\r\n" +
-               "Content-Type: text/plain\r\n" +
-               "Content-Length: #{response.bytesize}\r\n" +
-               "Connection: close\r\n")
+      client.print('\r\n')
 
-  client.print "\r\n"
-  client.print(response)
-  client.close
+      # write the contents of the file to the socket
+      IO.copy_stream(file, socket)
+    end
+
+  else
+    message = 'File not found\n'
+
+    # respond with a 404 error code to indicate the file does not exist
+    client.print('HTTP/1.1 404 Not Found\r\n' +
+                 'Content-Type: text/plain\r\n' +
+                 'Content-Length: #{message.size}\r\n' +
+                 'Connection: close\r\n')
+
+    socket.print('\r\n')
+
+    socket.print(message)
+  end
+
+  socket.close
 
 end
